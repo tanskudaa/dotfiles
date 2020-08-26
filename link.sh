e2a1d3ab6ff495eb6486980ed451c0aa7b96fb87
Taneli Hongisto <taneli.hongisto@protonmail.com>
2020-08-26 17:05:17 +0300

    Fixed VSCode snippets folder loop

---
+++ link.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Get absolute path
+DIR=$(cd `dirname $0` && pwd)
+
+# Link dotfiles
+ln -sf $DIR/bashrc $HOME/.bashrc
+ln -sf $DIR/bash_profile $HOME/.bash_profile
+ln -sf $DIR/Xresources $HOME/.Xresources
+ln -sf $DIR/inputrc $HOME/.inputrc
+
+ln -sf $DIR/bin -t $HOME
+
+ln -sf $DIR/i3 $HOME/.config/i3/config
+ln -sf $DIR/picom $HOME/.config/picom.conf
+
+ln -sf $DIR/vim .vim -t $HOME
+ln -sf $DIR/vimrc $HOME/.vimrc
+ln -sf $DIR/vscode/snippets -t $HOME/.config/'Code - OSS'/User/
+ln -sf $DIR/vscode/settings.json $HOME/.config/'Code - OSS'/User/settings.json
